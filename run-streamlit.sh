7e2ba40519938ca8da6d5be13200ad0629ec7793
Panchadip <165953910+Panchadip-128@users.noreply.github.com>
2024-07-07 02:57:00 +0530

    run-streamlit.sh

---
+++ run-streamlit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Change directory to where your Streamlit application script is located
+cd /home/user/repository
+
+# Run Streamlit app
+streamlit run app.py
